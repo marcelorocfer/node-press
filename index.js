57c3347cd7e37c4987ffa51c3f2c6456d91694e6
Marcelo Rocha Ferreira <marcelo@cpd.pmvc.ba.gov.br>
2020-11-23 14:30:39 -0300

    Base do Projeto

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database")
+
+// View Engine
+app.set('view engine', 'ejs');
+
+// Static
+app.use(express.static('public'));
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão realizada com sucesso!");
+    }).catch((err) => {
+        console.log(err);
+    });
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(8080, () => {
+    console.log("The server is running!");
+});

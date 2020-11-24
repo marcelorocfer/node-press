de4eba54a8bfdcedc4cc5d30047cfd3118f003c2
Marcelo Rocha Ferreira <marcelo@cpd.pmvc.ba.gov.br>
2020-11-24 16:07:00 -0300

    Salvando artigos.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# node-press
+# NodePress
+### Blog com painel administrativo desenvolvido com Node.js 
--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/articles", (req, res) => {
     res.send("Rota de Artigos");
@@ -9,6 +11,19 @@ router.get("/articles", (req, res) => {
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", { categories });
+    });
+});
+
+router.post("/articles/save", (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.create({
+        title,
+        body,
+        categoryId: category,
+        slug: slugify(title.toLowerCase()),
     });
 });
 

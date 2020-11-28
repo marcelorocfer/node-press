430c18caad62c969bbcfa5a1bc749d33b88f07d7
Marcelo Rocha Ferreira <marceloengecomp@gmail.com>
2020-11-28 15:12:28 -0300

    Criação do hash para salvar a senha no BD.

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
     res.send("Listagem de Usuários");
@@ -14,7 +15,18 @@ router.post("/users/create", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    res.json({email, password});
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email,
+        password: hash
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err) => {
+        res.redirect("/");
+        console.log(err);
+    });
 });
 
 module.exports = router;
